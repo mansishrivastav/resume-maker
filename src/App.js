b3c4efe78bc68e338a0a2348dca03b0c4a1b4a36
mansishrivastav <mansishrivastav04@gmail.com>
2023-07-23 19:34:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./components/header/Header";
+import Body from "./components/body/Body";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import resumeSvg from "../../assets/resume.svg";
+
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+        Create Your Perfect <span>Resume</span> effortlessly.
+        </p>
+        <p className={styles.heading}>
+        Try it <span>for Free!</span> 
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={resumeSvg} alt="Resume" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
